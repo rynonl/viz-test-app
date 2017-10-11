9bbd01b57e267ff0f4b29b930092286ae2d2c9f4
Ryan O'Neill <rco8786@gmail.com>
2017-10-11 14:54:50 -0400

    bs test

---
+++ __viz__/App.viz.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import App from '../src/App.js'
+
+export class AppTest1 extends Component {
+  render() {
+    return <App />
+  }
+}
+
+export class AppTest2 extends Component {
+  render() {
+    return <App />
+  }
+}
